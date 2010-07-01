ba8a6a3bc9a6c244ea47eb8c568c350b18547c15
Alexander Kurtakov <akurtako@redhat.com>
2010-07-01 22:43:27 +0300

    Handle scratch builds

---
+++ org.fedoraproject.eclipse.packager.koji/src/org/fedoraproject/eclipse/packager/koji/KojiScratchBuildHandler.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Red Hat Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat Inc. - initial API and implementation
+ *******************************************************************************/
+package org.fedoraproject.eclipse.packager.koji;
+
+
+public class KojiScratchBuildHandler extends KojiBuildHandler {
+
+	@Override
+	protected boolean isScratch() {
+		return true;
+	}
+}
