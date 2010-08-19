3204a0dc37c9bbd885e560eb43c1cb2e42ee0897
Severin Gehwolf <sgehwolf@redhat.com>
2010-08-19 17:31:01 -0400

    Modified tests for getBranchName() and getCurrentBranchName(). Modified GitTestProject accordingly.

--- org.fedoraproject.eclipse.packager.tests/src/org/fedoraproject/eclipse/packager/tests/git/GitFpBitsTest.java
+++ org.fedoraproject.eclipse.packager.tests/src/org/fedoraproject/eclipse/packager/tests/git/GitFpBitsTest.java
@@ -1,13 +1,23 @@
 package org.fedoraproject.eclipse.packager.tests.git;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.jgit.lib.Constants;
 import org.fedoraproject.eclipse.packager.git.FpGitProjectBits;
 import org.fedoraproject.eclipse.packager.handlers.FedoraHandlerUtils;
+import org.fedoraproject.eclipse.packager.test_utils.GitTestProject;
 
 public class GitFpBitsTest extends GitTestCase {
 
-	public void testGetCurrentBranchName() {
-		fail("Not yet implemented");
+	public void testGetCurrentBranchName() throws Exception {
+		IProject project = getiProject();
+		FpGitProjectBits projectBits = (FpGitProjectBits)FedoraHandlerUtils.getVcsHandler(project);
+		assertNotNull(projectBits);
+		// make sure we meet pre-condition (we should be on master)
+		assertEquals("devel", projectBits.getCurrentBranchName());
+		GitTestProject testProject = getProject();
+		// switch to remote branch f13
+		testProject.checkoutBranch(Constants.R_HEADS + "f13/master");
+		assertEquals("F-13", projectBits.getCurrentBranchName());
 	}
 	
 	public void testGetBranchName() {
