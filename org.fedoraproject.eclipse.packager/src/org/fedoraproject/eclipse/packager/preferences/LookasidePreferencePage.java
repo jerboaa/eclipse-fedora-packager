df9465207361e1ec295dfba4ffe2bdbd93c6e58c
Severin Gehwolf <sgehwolf@redhat.com>
2010-09-07 16:55:10 -0400

    Fix copyright.

--- org.fedoraproject.eclipse.packager/src/org/fedoraproject/eclipse/packager/preferences/LookasidePreferencePage.java
+++ org.fedoraproject.eclipse.packager/src/org/fedoraproject/eclipse/packager/preferences/LookasidePreferencePage.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2007 Alphonse Van Assche.
+ * Copyright (c) 2010 Red Hat Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Alphonse Van Assche - initial API and implementation
+ *     Red Hat Inc. - initial API and implementation
  *******************************************************************************/
 
 package org.fedoraproject.eclipse.packager.preferences;
