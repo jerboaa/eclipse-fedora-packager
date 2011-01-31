ca3c09cf0d4735654ca66774ad51c59a9fa1f5bb
Severin Gehwolf <sgehwolf@redhat.com>
2011-01-31 11:28:48 -0500

    Fix tests plug-in bundle activation (old activator class was erroneously removed).

---
+++ org.fedoraproject.eclipse.packager.tests/src/org/fedoraproject/eclipse/packager/tests/TestsPlugin.java
@@ -0,0 +1,49 @@
+package org.fedoraproject.eclipse.packager.tests;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+public class TestsPlugin extends AbstractUIPlugin {
+
+	/** The symbolic ID of the plugin	 */
+	public static final String PLUGIN_ID = "org.fedoraproject.eclipse.packager.tests"; //$NON-NLS-1$
+
+	// The shared instance
+	private static TestsPlugin plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public TestsPlugin() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static TestsPlugin getDefault() {
+		return plugin;
+	}
+
+}
