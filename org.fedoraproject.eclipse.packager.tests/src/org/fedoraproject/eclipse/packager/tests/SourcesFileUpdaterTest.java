2533de3e848ba8a9876c2ba04ba3aaa429b97bfa
Severin Gehwolf <sgehwolf@redhat.com>
2011-02-28 15:50:31 -0500

    Add test stub for sources file updater.

---
+++ org.fedoraproject.eclipse.packager.tests/src/org/fedoraproject/eclipse/packager/tests/SourcesFileUpdaterTest.java
@@ -0,0 +1,34 @@
+package org.fedoraproject.eclipse.packager.tests;
+
+import static org.junit.Assert.*;
+
+import org.fedoraproject.eclipse.packager.api.SourcesFileUpdater;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test for {@code sources} file updater, {@link SourcesFileUpdater}.
+ *
+ */
+public class SourcesFileUpdaterTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void canReplaceSourcesFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void canUpdateSourcesFile() {
+		fail("Not yet implemented");
+	}
+
+}
