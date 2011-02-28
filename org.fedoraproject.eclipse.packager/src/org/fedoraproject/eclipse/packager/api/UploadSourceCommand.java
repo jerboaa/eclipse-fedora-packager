c97672fd155e3293f60c69db95baddc1365d4b40
Severin Gehwolf <sgehwolf@redhat.com>
2011-02-28 17:03:38 -0500

    Implement upload source command.

--- org.fedoraproject.eclipse.packager/src/org/fedoraproject/eclipse/packager/api/UploadSourceCommand.java
+++ org.fedoraproject.eclipse.packager/src/org/fedoraproject/eclipse/packager/api/UploadSourceCommand.java
@@ -3,6 +3,7 @@ package org.fedoraproject.eclipse.packager.api;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.MessageFormat;
 
 import org.apache.http.HttpResponse;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -10,8 +11,12 @@ import org.fedoraproject.eclipse.packager.FedoraPackagerText;
 import org.fedoraproject.eclipse.packager.FedoraProjectRoot;
 import org.fedoraproject.eclipse.packager.LookasideCache;
 import org.fedoraproject.eclipse.packager.SourcesFile;
+import org.fedoraproject.eclipse.packager.api.errors.CommandListenerException;
+import org.fedoraproject.eclipse.packager.api.errors.CommandMisconfiguredException;
 import org.fedoraproject.eclipse.packager.api.errors.FedoraPackagerAPIException;
 import org.fedoraproject.eclipse.packager.api.errors.FileAvailableInLookasideCacheException;
+import org.fedoraproject.eclipse.packager.api.errors.InvalidUploadFileException;
+import org.fedoraproject.eclipse.packager.utils.FedoraPackagerUtils;
 
 /**
  * A class used to execute a {@code upload sources} command. It has setters for
@@ -51,10 +56,22 @@ public class UploadSourceCommand extends
 	}
 
 	/**
-	 * @param fileToUpload the fileToUpload to set. Required.
+	 * Setter for the file to be uploaded.
+	 * 
+	 * @param fileToUpload
+	 *            The file to be uploaded.
 	 * @return this instance.
+	 * @throws InvalidUploadFileException
+	 *             If the upload file candidate is an invalid file.
 	 */
-	public UploadSourceCommand setFileToUpload(File fileToUpload) {
+	@SuppressWarnings("static-access")
+	public UploadSourceCommand setFileToUpload(File fileToUpload)
+			throws InvalidUploadFileException {
+		if (!FedoraPackagerUtils.isValidUploadFile(fileToUpload)) {
+			throw new InvalidUploadFileException(MessageFormat.format(
+					FedoraPackagerText.get().uploadSourceCommand_uploadFileInvalid,
+					fileToUpload.getName()));
+		}
 		this.fileToUpload = fileToUpload;
 		return this;
 	}
@@ -75,12 +92,28 @@ public class UploadSourceCommand extends
 	 * 
 	 * @throws FileAvailableInLookasideCacheException If the to-be-uploaded file
 	 * is still missing from the lookaside cache.
+	 * @throws CommandMisconfiguredException If the command was not properly configured.
+	 * @throws CommandListenerException If a listener caused an error.
 	 */
 	@Override
 	public UploadSourceResult call(IProgressMonitor monitor)
-		throws FileAvailableInLookasideCacheException {
-		//TODO implement
-		return null;
+		throws FileAvailableInLookasideCacheException, CommandMisconfiguredException, CommandListenerException {
+		try {
+			callPreExecListeners();
+		} catch (CommandListenerException e) {
+			if (e.getCause() instanceof CommandMisconfiguredException) {
+				// explicitly throw the specific exception
+				throw (CommandMisconfiguredException)e.getCause();
+			}
+			throw e;
+		}
+		// implementation
+		UploadSourceResult result = new UploadSourceResult();
+		
+		
+		callPostExecListeners();
+		result.setSuccessful(true);
+		return result;
 	}
 	
 	@SuppressWarnings("static-access")
---
+++ org.fedoraproject.eclipse.packager/src/org/fedoraproject/eclipse/packager/api/errors/InvalidUploadFileException.java
@@ -0,0 +1,26 @@
+package org.fedoraproject.eclipse.packager.api.errors;
+
+import org.fedoraproject.eclipse.packager.utils.FedoraPackagerUtils;
+
+/**
+ * Thrown if an upload file is invalid as determined by
+ * {@link FedoraPackagerUtils#isValidUploadFile(java.io.File)}.
+ */
+public class InvalidUploadFileException extends FedoraPackagerAPIException {
+	
+	private static final long serialVersionUID = -7299319057739321380L;
+	
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public InvalidUploadFileException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	/**
+	 * @param message
+	 */
+	public InvalidUploadFileException(String message) {
+		super(message);
+	}
+}
