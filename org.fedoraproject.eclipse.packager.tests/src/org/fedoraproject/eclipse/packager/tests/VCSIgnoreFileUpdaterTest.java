923d12b516450c999f4147be8eba683844bd6d8a
Severin Gehwolf <sgehwolf@redhat.com>
2011-02-28 15:53:47 -0500

    Add VCS ignore file updater test stub.

---
+++ org.fedoraproject.eclipse.packager.tests/src/org/fedoraproject/eclipse/packager/tests/VCSIgnoreFileUpdaterTest.java
@@ -0,0 +1,34 @@
+package org.fedoraproject.eclipse.packager.tests;
+
+import static org.junit.Assert.*;
+
+import org.fedoraproject.eclipse.packager.api.VCSIgnoreFileUpdater;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test for VCS ignore file updater, {@link VCSIgnoreFileUpdater}.
+ *
+ */
+public class VCSIgnoreFileUpdaterTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void canReplaceContentWithNewIgnoreContent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void canAppendToIgnoreFileContent() {
+		fail("Not yet implemented");
+	}
+
+}
