25e7ce4b3328ea7a5459643e7ca87f65ebd48344
Severin Gehwolf <sgehwolf@redhat.com>
2011-07-22 17:27:43 -0400

    Added deserialization model classes for Bodhi updates.
    Corresponding unit tests have been added as well.

---
+++ org.fedoraproject.eclipse.packager.bodhi/src/org/fedoraproject/eclipse/packager/bodhi/api/deserialization/BodhiUpdate.java
@@ -0,0 +1,177 @@
+package org.fedoraproject.eclipse.packager.bodhi.api.deserialization;
+
+import com.google.gson.JsonObject;
+
+/**
+ * Class which represents a pushed Bodhi update. Deserialized from
+ * a JSON String by google.gson.
+ *
+ */
+public class BodhiUpdate {
+
+	private String status;
+	private boolean close_bugs;
+	private String request;
+	private String date_submitted;
+	private int unstable_karma;
+	private String submitter;
+	private boolean critpath;
+	private JsonObject approved;
+	private int stable_karma;
+	private String date_pushed;
+	private Build[] builds;
+	// nvr of the update
+	private String title;
+	// update comment
+	private String notes;
+	private String date_modified;
+	private JsonObject nagged;
+	private int[] bugs;
+	private JsonObject[] comments;
+	private boolean critpath_approved;
+	private int updateid;
+	private int karma;
+	// TODO: Model release appropriately
+	private JsonObject release;
+	private String type;
+	
+	/**
+	 * Google GSON wants this
+	 */
+	public BodhiUpdate() {
+		// nothing
+	}
+	
+	/**
+	 * @return the status
+	 */
+	public String getStatus() {
+		return status;
+	}
+	/**
+	 * @return the close_bugs
+	 */
+	public boolean isCloseBugs() {
+		return close_bugs;
+	}
+	/**
+	 * @return the request
+	 */
+	public String getRequest() {
+		return request;
+	}
+	/**
+	 * @return the date_submitted
+	 */
+	public String getDateSubmitted() {
+		return date_submitted;
+	}
+	/**
+	 * @return the unstable_karma
+	 */
+	public int getUnstableKarma() {
+		return unstable_karma;
+	}
+	/**
+	 * @return the submitter
+	 */
+	public String getSubmitter() {
+		return submitter;
+	}
+	/**
+	 * @return the critpath
+	 */
+	public boolean isCritpath() {
+		return critpath;
+	}
+	/**
+	 * @return the approved
+	 */
+	public JsonObject getApproved() {
+		return approved;
+	}
+	/**
+	 * @return the stable_karma
+	 */
+	public int getStable_karma() {
+		return stable_karma;
+	}
+	/**
+	 * @return the date_pushed
+	 */
+	public String getDatePushed() {
+		return date_pushed;
+	}
+	/**
+	 * @return the builds
+	 */
+	public Build[] getBuilds() {
+		return builds;
+	}
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	/**
+	 * @return the notes
+	 */
+	public String getNotes() {
+		return notes;
+	}
+	/**
+	 * @return the date_modified
+	 */
+	public String getDateModified() {
+		return date_modified;
+	}
+	/**
+	 * @return the nagged
+	 */
+	public JsonObject getNagged() {
+		return nagged;
+	}
+	/**
+	 * @return the bugs
+	 */
+	public int[] getBugs() {
+		return bugs;
+	}
+	/**
+	 * @return the comments
+	 */
+	public JsonObject[] getComments() {
+		return comments;
+	}
+	/**
+	 * @return the critpath_approved
+	 */
+	public boolean isCritpathApproved() {
+		return critpath_approved;
+	}
+	/**
+	 * @return the updateid
+	 */
+	public int getUpdateid() {
+		return updateid;
+	}
+	/**
+	 * @return the karma
+	 */
+	public int getKarma() {
+		return karma;
+	}
+	/**
+	 * @return the release
+	 */
+	public JsonObject getRelease() {
+		return release;
+	}
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+}
---
+++ org.fedoraproject.eclipse.packager.tests/src/org/fedoraproject/eclipse/packager/tests/units/deserialization/BodhiUpdateResponseTest.java
@@ -0,0 +1,78 @@
+package org.fedoraproject.eclipse.packager.tests.units.deserialization;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.fedoraproject.eclipse.packager.bodhi.api.BodhiUpdateResponse;
+import org.junit.Test;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class BodhiUpdateResponseTest {
+
+	private static final String EXAMPLE_UPDATE_JSON_RESPONSE =
+			"{"+
+					" \"tg_flash\": \"Update successfully created\","+
+					" \"updates\":"+
+					"    [ "+
+					"       { \"status\": \"pending\","+
+					"         \"close_bugs\": false,"+
+					"	 \"request\": \"testing\","+
+					"	 \"date_submitted\": \"2011-07-22 19:06:59\","+
+					"	 \"unstable_karma\": -3,"+
+					"	 \"submitter\": \"guest\","+
+					"	 \"critpath\": false,"+
+					"	 \"approved\": null,"+
+					"	 \"stable_karma\": 3,"+
+					"	 \"date_pushed\": null,"+
+					"	 \"builds\":"+
+					"	    ["+
+					"	      { \"nvr\": \"ed-1.5-3.fc15\","+
+					"	        \"package\":"+
+					"		  { \"suggest_reboot\": false,"+
+					"		    \"committers\": [\"guest\"],"+
+					"		    \"name\": \"ed\""+
+					"		  }"+
+					"              }"+
+					"	    ],"+
+					"	 \"title\": \"ed-1.5-3.fc15\","+
+					"	 \"notes\": \"This is a test. Please disregard\","+
+					"	 \"date_modified\": null,"+
+					"	 \"nagged\": null,"+
+					"	 \"bugs\": [],"+
+					"	 \"comments\": "+
+					"	    [ {"+
+					"	        \"group\": null,"+
+					"		\"karma\": 0,"+
+					"		\"anonymous\": false,"+
+					"		\"author\": \"bodhi\","+
+					"		\"timestamp\": \"2011-07-22 19:07:02\","+
+					"		\"text\": \"This update has been submitted for testing by guest.\""+
+					"	      } ],"+
+					"	 \"critpath_approved\": false,"+
+					"	 \"updateid\": null,"+
+					"	 \"karma\": 0,"+
+					"	 \"release\":"+
+					"	   { "+
+					"	     \"dist_tag\": \"dist-f15\","+
+					"	     \"id_prefix\": \"FEDORA\","+
+					"	     \"locked\": true,"+
+					"	     \"name\": \"F15\","+
+					"	     \"long_name\": \"Fedora 15\""+
+					"	   },"+
+					"	 \"type\": \"enhancement\""+
+					"       }"+
+					"   ]"+
+					"}";
+	
+	@Test
+	public void canDeserializeUpdateJSONResponse() throws Exception {
+		GsonBuilder gsonBuilder = new GsonBuilder();
+		Gson gson = gsonBuilder.create();
+		BodhiUpdateResponse result = gson.fromJson(EXAMPLE_UPDATE_JSON_RESPONSE, BodhiUpdateResponse.class);
+		assertNotNull(result);
+		assertEquals("Update successfully created", result.getFlashMsg());
+		assertEquals("ed", result.getUpdates()[0].getBuilds()[0].getPkg().getName());
+	}
+}
